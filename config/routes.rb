0a914b65fc514ef87933d003af6939ade6926d3e
Bruna Guedes <bruna.guedes02@gmail.com>
2020-11-26 16:24:09 -0300

    As a user, I can add a new task

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # As a user, I can list tasks
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
 end
